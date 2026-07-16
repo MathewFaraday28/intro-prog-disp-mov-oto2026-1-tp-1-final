dbe9bcfcdfa665de9364da3ef2c9234a7f34bf67
alienciso21-hub <alienciso21@gmail.com>
2026-07-16 15:59:50 -0300

    Update Libro.kt

--- app/src/main/java/com/tpbiblioteca/registrolibros/model/Libro.kt
+++ app/src/main/java/com/tpbiblioteca/registrolibros/model/Libro.kt
@@ -1,10 +1,5 @@
 package com.tpbiblioteca.registrolibros.model
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.tpbiblioteca.registrolibros.ui.ListaLibros
-import com.tpbiblioteca.registrolibros.ui.theme.RegistroLibrosTheme
-
 data class Libro(
     val id: String,
     val titulo: String,
